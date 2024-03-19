7f372ec0f701ac79b5664f62057e6a5565ee1ff3
vi-8086 <102787315+vi-8086@users.noreply.github.com>
2024-03-19 14:54:25 +0530

    Update extract_files.py

--- data-preparation/extract_files.py
+++ data-preparation/extract_files.py
@@ -4,21 +4,24 @@ import os
 def extract_files(zip_file, extract_dir):
     extracted_files = []
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        # Check if the Dataset directory exists in the zip file
-        if 'Dataset' not in zip_ref.namelist():
-            print("Error: Dataset directory not found in the zip file.")
-            return
-        
-        # Extract files from the Dataset directory
-        for file_info in zip_ref.infolist():
-            if file_info.filename.startswith('Dataset/') and not file_info.is_dir():
-                extracted_files.append(file_info.filename.split('/', 1)[1])  # Extract the file name without the 'Dataset/' prefix
-                zip_ref.extract(file_info, extract_dir)
+        # Extract all files and directories from the zip file
+        zip_ref.extractall(extract_dir)
+        # Iterate over the extracted files and directories
+        for root, dirs, files in os.walk(extract_dir):
+            for file in files:
+                # Remove the prefix "Dataset/" from the file path
+                file_path = os.path.join(root, file)
+                new_file_path = file_path.replace('Dataset/', '')
+                extracted_files.append(new_file_path)
         print("Files extracted successfully.")
     return extracted_files
 
 if __name__ == "__main__":
-    zip_file = "DFDC.zip"  # Replace with the name of your zip file
-    extract_dir = "extracted_files"  # Replace with the directory where you want to extract files
+    zip_file = "/content/drive/MyDrive/Colab Notebooks/DeepFake Image Detection/DFDC.zip"  # Replace with the path to your zip file
+    extract_dir = "/content/extracted_files"  # Replace with the directory where you want to extract files
     extracted_files = extract_files(zip_file, extract_dir)
     print("Extracted files:", extracted_files)
+
+
+
+
