1340f2bc1f1aee94b32aa2717fa35a5c16110624
vi-8086 <102787315+vi-8086@users.noreply.github.com>
2024-03-19 14:11:37 +0530

    Create extract_files.py

---
+++ data-preparation/extract_files.py
@@ -0,0 +1,23 @@
+import shutil
+
+# Get the path to the extracted 'Dataset' directory
+extracted_dataset_directory = os.path.join(extract_to_directory, 'Dataset')
+
+# List the contents of the 'Dataset' directory
+dataset_contents = os.listdir(extracted_dataset_directory)
+
+# Move each item from the 'Dataset' directory to the extraction directory
+for item in dataset_contents:
+    # Get the full path of the item
+    item_path = os.path.join(extracted_dataset_directory, item)
+    
+    # Move the item to the extraction directory
+    shutil.move(item_path, extract_to_directory)
+
+# Remove the empty 'Dataset' directory
+os.rmdir(extracted_dataset_directory)
+
+# List the extracted files again
+extracted_files = os.listdir(extract_to_directory)
+print("Extracted files:", extracted_files)
+
